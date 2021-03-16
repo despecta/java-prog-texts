97b06491797b97870c4288173040cf9cc2f19fdd
Adriel Ernesto <73506151+despecta@users.noreply.github.com>
2021-03-16 11:53:11 -0300

    Add files via upload

---
+++ exer001.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Despecta est
+ */
+public class exer001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int anoFundacao, quantidadeHabitantes; 
+        String nomeCidade;
+        nomeCidade = "Feira de salvador";
+        anoFundacao = 1877;
+        quantidadeHabitantes = 5000; 
+        System.out.println(nomeCidade);
+        System.out.println(anoFundacao);
+        System.out.println(quantidadeHabitantes);
+    }
+    
+}
